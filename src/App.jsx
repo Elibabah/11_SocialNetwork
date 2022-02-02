7b835f702295c224b07a08ae3419acc81211448d
Elibabah <delias16.hernandez@gmail.com>
2022-02-02 17:12:55 -0600

    new template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css';
+import React from "react"
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home"
+import Register from "./components/Register"
+import { AuthProvider } from "./context/authContext"
 
 function App() {
     return ( 
-        <div>
-            New deploy
-        </div>
+<div className="bg-slate-300 h-screen flex">
+    <AuthProvider>
+        <Routes>
+            <Route path="11_SocialNetwork/register" element={<Register/>} />
+            <Route path="11_SocialNetwork/" element={<Home />} />
+        </Routes>
+    </AuthProvider>
+    </div>
     );
 }
 
