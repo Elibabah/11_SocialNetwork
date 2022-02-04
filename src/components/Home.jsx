e309fa38fe54f04c18e6f15eb9a854a28f0c1d6e
Elibabah <delias16.hernandez@gmail.com>
2022-02-04 17:35:02 -0600

    fixing path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -43,12 +43,11 @@ export function Home() {
 
             <form onSubmit={handleSubmit} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
             <div className='mb-4'>
-                  <label htmlFor="title" className='block text-gray-700 text-sm font-fold mb-2'>Dale nombre a tu historia</label>
+                  <label htmlFor="title" className='block text-gray-700 text-sm font-fold mb-2'>Cuéntanos tu historia</label>
                   <input type="text" name="title" id="title" placeholder="título" className='shadow appareance-non border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
             </div>
 
             <div className='mb-4'>
-                  <label htmlFor="story" className='block text-gray-700 text-sm font-fold mb-2'>Cuéntanos tu historia</label>
                   <input type="text" name="story" id="story" placeholder="Escribe tu historia" className='shadow appareance-non border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'className='shadow appareance-non border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
             </div>
             <div className='flex items-center justify-between'>
